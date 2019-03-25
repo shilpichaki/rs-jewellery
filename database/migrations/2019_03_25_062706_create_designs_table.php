e6086de0442e0938a00c9ca72c3b61606cd72149
Suman Sarkar <sumansarkarwd@gmail.com>
2019-03-25 13:45:02 +0530

    Design add complete with backend validation

---
+++ database/migrations/2019_03_25_062706_create_designs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDesignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('design_no')->unique()->unsigned();
+            $table->text('stones');
+            $table->string('picture')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('designs');
+    }
+}
