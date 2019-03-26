bcde57d31de1130adc3a3a82b4185e0b8d2dfd1f
Suman Sarkar <sumansarkarwd@gmail.com>
2019-03-26 13:07:08 +0530

    Design add front-end validations done

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/stock/{stock}', 'Api\StockController@show');
+Route::get('/design/{design}', 'Api\DesignController@show');
