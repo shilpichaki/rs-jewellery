dbc8e376db1abb46d8b49492505840ba24059612
Suman Sarkar <sumansarkarwd@gmail.com>
2019-04-01 22:43:01 +0530

    Removied database seeding for stock

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,60 +20,6 @@ class DatabaseSeeder extends Seeder
 
         Stock::truncate();
 
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::WAX,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::INVESTMENT_POWDER,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::BRASS_METAL,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::ROUND_STONE,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::BIG_STONE,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::TOOLS,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::CHEMICAL,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::PACKAGING_MATERIAL,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
-        Stock::create([
-            'raw_material_type' => \App\Enums\RawMaterial::RUBBER,
-            'threshold_value' => (mt_rand(1, 5) * 50),
-            'stock_value' => mt_rand(50, 100) * 50
-        ]);
-
         User::truncate();
         
         User::create([
