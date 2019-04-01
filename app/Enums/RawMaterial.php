f7d06b1f8b1c8d293a733851aa49beddb5805428
Suman sarkar <sumansarkarwd@gmail.com>
2019-04-01 15:55:50 +0530

    Changed API for getting RawMaterials and UnitOfMesurements
    Both Rawmaterial and UnitOfMesurement will have a single API to get a list of raw materials and uoms accordingly.
    
    Like: RawMaterial::getAllRawMaterials() & UnitOfMeasurement::getUomList()

--- app/Enums/RawMaterial.php
+++ app/Enums/RawMaterial.php
@@ -5,14 +5,52 @@ namespace App\Enums;
 
 abstract class RawMaterial
 {
-    const WAX = 'WAX';
-    const INVESTMENT_POWDER = 'INVESTMENT POWDER';
-    const BRASS_METAL = 'BRASS METAL';
-    const ROUND_STONE = 'ROUND STONE';
-    const BIG_STONE = 'BIG STONE';
-    const TB_STONE = 'TB STONE';
-    const TOOLS = 'TOOLS';
-    const CHEMICAL = 'CHEMICAL';
-    const PACKAGING_MATERIAL = 'PACKAGING MATERIAL';
-    const RUBBER = 'RUBBER';
+    private const WAX = 'WAX';
+    private const INVESTMENT_POWDER = 'INVESTMENT POWDER';
+    private const BRASS_METAL = 'BRASS METAL';
+    private const ROUND_STONE = 'ROUND STONE';
+    private const TB_STONE = 'TB STONE';
+    private const BIG_STONE = 'BIG STONE';
+    private const TOOLS = 'TOOLS';
+    private const CHEMICAL = 'CHEMICAL';
+    private const PACKAGING_MATERIAL = 'PACKAGING MATERIAL';
+    private const RUBBER = 'RUBBER';
+
+    private static $rawMaterials = [];
+    private static $stoneTypes = [];
+
+    private static function setRawMaterialList()
+    {
+    	array_push(self::$rawMaterials, self::WAX);
+        array_push(self::$rawMaterials, self::INVESTMENT_POWDER);
+        array_push(self::$rawMaterials, self::BRASS_METAL);
+        array_push(self::$rawMaterials, self::ROUND_STONE);
+        array_push(self::$rawMaterials, self::BIG_STONE);
+        array_push(self::$rawMaterials, self::TB_STONE);
+        array_push(self::$rawMaterials, self::TOOLS);
+        array_push(self::$rawMaterials, self::RUBBER);
+        array_push(self::$rawMaterials, self::CHEMICAL);
+        array_push(self::$rawMaterials, self::PACKAGING_MATERIAL);
+    }
+
+    public static function getAllRawMaterials()
+    {
+    	self::setRawMaterialList();
+
+    	return self::$rawMaterials;
+    }
+
+    private static function setStoneTypes()
+    {
+        array_push(self::$stoneTypes, self::ROUND_STONE);
+        array_push(self::$stoneTypes, self::BIG_STONE);
+        array_push(self::$stoneTypes, self::TB_STONE);
+    }
+
+    public static function getStoneTypes()
+    {
+    	self::setStoneTypes();
+
+    	return self::$stoneTypes;
+    }
 }
